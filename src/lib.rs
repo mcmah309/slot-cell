1ed84f43f2b8c7cdb94a1970d66dcb1310f4b265
mcmah309 <dillonmcmahon79@gmail.com>
2025-12-19 04:02:32 +0000

    Initial impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,59 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use core::cell::Cell;
+
+struct SlotCell<T> {
+    cell: Cell<Option<T>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<T> SlotCell<T> {
+    pub const fn new(val: T) -> Self {
+        Self {
+            cell: Cell::new(Some(val)),
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    pub const fn late() -> Self {
+        Self {
+            cell: Cell::new(None),
+        }
+    }
+
+    pub fn take(&self) -> T {
+        self.cell.take().expect("The value has already been taken")
+    }
+
+    pub fn put(&self, val: T) {
+        // SAFETY: This type is !Sync and no other references to the interior data will exist
+        // at this point. So it is fine to temporarily check the interior contents without moving out.
+        unsafe {
+            if (*self.cell.as_ptr()).is_none() {
+                panic!("The value had already been put back or was never taken")
+            }
+        }
+        let _ = self.cell.replace(Some(val));
+    }
+
+    pub fn set(&self, val: T) {
+        // SAFETY: This type is !Sync and no other references to the interior data will exist
+        // at this point. So it is fine to temporarily check the interior contents without moving out.
+        unsafe {
+            if (*self.cell.as_ptr()).is_none() {
+                panic!("Value set while already aquired")
+            }
+        }
+        let _ = self.cell.replace(Some(val));
+    }
+
+    pub fn swap(&self, other: &Self) {
+        // SAFETY: This type is !Sync and no other references to the interior data will exist
+        // at this point. So it is fine to temporarily check the interior contents without moving out.
+        unsafe {
+            if (*self.cell.as_ptr()).is_none() {
+                panic!("self is already aquired")
+            }
+            if (*other.cell.as_ptr()).is_none() {
+                panic!("other is already aquired")
+            }
+        }
+        self.cell.swap(&other.cell);
     }
 }
